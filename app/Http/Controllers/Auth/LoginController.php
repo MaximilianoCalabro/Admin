902d624c369e065bb47cf409986657d060a32f8e
MaximilianoCalabro <maxi_calabro@hotmail.com>
2018-04-03 08:00:39 -0300

    za
    za

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'layouts.admin';
+    protected $redirectTo = '/slider/configurar_slider';
 
     /**
      * Create a new controller instance.
